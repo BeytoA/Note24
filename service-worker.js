70f43aafe68c591705118b02ad4db55b09b7a616
BeytoA <67140682+BeytoA@users.noreply.github.com>
2023-08-22 02:22:17 +0300

    Attempt to solve offline page loading issues

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,22 @@
-const cacheName = "note24-cache-2";
+const cacheName = "note24-cache-3";
 const staticAssets = [
     "./",
     "./index.html",
-    "./logo.png",
+    "./logo256x256.png",
+    "./manifest.json",
     "./views/defaultStyle.css",
     "./views/fa-regular-400.ttf",
     "./views/fa-solid-900.ttf",
-    new Request("./views/notePage.html"),
+    "./views/notePage.html",
     "./views/notePageStyle.css",
     "./views/OpenSans-Light.ttf",
     "./views/OpenSans-Regular.ttf",
     "./models/generalDataModel.js",
     "./controllers/notePageController.js"
+];
+
+const ignoreQueriesForThesePages = [
+    "notePage.html"
 ];
 
 self.addEventListener("install", async e => {
@@ -24,13 +29,36 @@ self.addEventListener("activate", e => {
 });
 
 self.addEventListener("fetch", async e => {
-    const req = e.request;
-    const url = new URL(req.url);
+    //console.log("Fetch Request:" + e.request.url);
+    //Get the requested URL
+    let urlString = e.request.url;
 
+    //Check if the query string should be ignored for this page
+    for (i = 0; i < ignoreQueriesForThesePages.length; i++) {
+        if ( urlString.indexOf(ignoreQueriesForThesePages[i]) > -1) {
+            urlString = urlString.split(ignoreQueriesForThesePages[i])[0] + ignoreQueriesForThesePages[i];
+            break;
+        }
+    }
+
+    //Create a new request based on the original request
+    var requestToFetch = new Request(urlString, {
+        body: e.request.body,
+        method: e.request.method,
+        headers: e.request.headers,
+        mode: "same-origin",
+        credentials: e.request.credentials,
+        redirect: e.request.redirect
+    });
+    
+    //console.log("Fetch After:" + requestToFetch.url);
+    const url = new URL(urlString);
+
+    //Serve a response from cache or network
     if (url.origin == location.origin) {
-        e.respondWith(cacheFirst(req));
+        e.respondWith(cacheFirst(requestToFetch));
     } else {
-        e.respondWith(networkAndCache(req));
+        e.respondWith(networkAndCache(requestToFetch));
     }
 });
 
